4fb9ef3542797daea3caa08e20770295bbaa5d9e
Andre Telfer <telfer006@gmail.com>
2020-02-08 18:32:06 -0500

    Added react body

--- web/src/Components/App.js
+++ web/src/Components/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
+import {ReactComponent as BodySvg} from '../Assets/body-part.svg'
 
 export default ()=> (
   <ThemeProvider theme={Theme}>
     <GlobalStyles/>
+    <BodySvg />
   </ThemeProvider>
 );
