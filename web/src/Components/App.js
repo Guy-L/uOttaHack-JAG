faa691bcc1efef0437d02a02312356198b8fe09c
Jeeheon Kim <jeeheonkim0@gmail.com>
2020-02-08 18:34:00 -0500

    added a nav

--- web/src/Components/App.js
+++ web/src/Components/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { ThemeProvider } from "styled-components";
+import styled,{ ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Form, FormControl, Button} from 'react-bootstrap';
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%
+`;
 
 export default ()=> (
   <ThemeProvider theme={Theme}>
     <GlobalStyles/>
+    <Navbar>
+      <Navbar.Brand href="#home" class="logo">JagtMove Analytics</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Signed in as: <a href="#login">Dr.Mark Otto</a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+    <br/>
+
   </ThemeProvider>
 );
--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
+    <App/>,
     document.getElementById("root")
 );
