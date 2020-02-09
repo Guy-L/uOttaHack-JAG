9c84b174922255148039baf268607168e78ea9bf
Jeeheon Kim <jeeheonkim0@gmail.com>
2020-02-08 20:56:38 -0500

    some changes

--- web/src/Components/App.js
+++ web/src/Components/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
 import styled,{ ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Form, FormControl, Button} from 'react-bootstrap';
 import {ReactComponent as BodySvg} from '../Assets/body-part.svg'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, { useState, useRef, useEffect } from "react";
+import {Radar} from 'react-chartjs-2';
+import { Card} from 'reactstrap';
 
 const Wrapper = styled.div`
   margin: 0 auto;
@@ -12,11 +15,33 @@ const Wrapper = styled.div`
   width: 100%
 `;
 
+const Input = styled.input`
+  font-size: 14px;
+  line-height: 1;
+  background-color: transparent;
+  width: 100%;
+  margin-left: ${props => (props.barOpened ? "1rem" : "0rem")};
+  border: none;
+  color: white;
+  transition: margin 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+  &::placeholder {
+    color: white;
+  }
+`;
+
+
 export default ()=> (
   <ThemeProvider theme={Theme}>
-    <GlobalStyles/>
-    <Navbar>
-      <Navbar.Brand href="#home" class="logo">JagtMove Analytics</Navbar.Brand>
+    <Navbar color="light" light id="navigation-bar">
+      <Navbar.Brand href="#home" id="brand" >
+        <img src="https://img.icons8.com/color/48/000000/sports-mode.png"></img>
+        JagtMove Analytics
+      </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
@@ -26,8 +51,14 @@ export default ()=> (
     </Navbar>
     <br/>
 
-    <body>
-      <BodySvg/>
-    </body>
+    <div id="content">
+      <Card>
+        <BodySvg/>
+      </Card>
+      <Card>
+        <Radar>
+        </Radar>
+      </Card>
+    </div>
   </ThemeProvider>
 );
--- web/src/Styles/GlobalStyles.js
+++ web/src/Styles/GlobalStyles.js
@@ -13,11 +13,29 @@ export default createGlobalStyle`
         font-size:14px;
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
+
+    #content{
+        padding: 40px;
+    }
+
     a {
         color:${props => props.theme.blueColor};
         text-decoration:none;
     }
     input:focus{
         outline:none;
+    }
+
+    #navigation-bar {
+        -webkit-box-shadow: 2px 2px 1px #000000;
+        -moz-box-shadow:    2px 2px 1px #000000;
+        box-shadow:         1px 1px 1px #000000;
+        z-index:999;
+        position: fixed; /* Set the navbar to fixed position */
+    }
+
+    #brand{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;  
+        font-style: italic;      
     }
 `;
